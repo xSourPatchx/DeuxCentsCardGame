96b29692719900d0c2a3bcbd557690f7a8875a2c
xSourPatchx <patsurette93@gmail.com>
2025-10-11 01:22:23 +0000

    Reviewed failed tests

--- TempFileCanDelete.cs
+++ TempFileCanDelete.cs
@@ -1,3 +1,22 @@
+// Failed tests
+DeuxCentsCardGame.Tests.Managers.BettingManagerTests.ExecuteBettingRound_WithInvalidBet_ShowsError
+DeuxCentsCardGame.Tests.Managers.BettingManagerTests.ExecuteBettingRound_WithMaxBet_EndsImmediately
+DeuxCentsCardGame.Tests.Managers.BettingManagerTests.ExecuteBettingRound_WithValidBet_ProcessesCorrectly
+DeuxCentsCardGame.Tests.Managers.BettingManagerTests.ExecuteBettingRound_ThreePassesForcesLastPlayerToBet50
+
+DeuxCentsCardGame.Tests.Managers.DealingManagerTests.RaiseCardsDealtEvent_CallsEventManager
+
+DeuxCentsCardGame.Tests.Managers.DeckManagerTests.ShuffleDeck_ChangesCardOrder
+
+DeuxCentsCardGame.Tests.Managers.ScoringManagerTests.IsGameOver_WhenTeamReaches200_ReturnsTrue
+
+DeuxCentsCardGame.Tests.Managers.TrumpSelectionManagerTests.SelectTrumpSuit_VariousInputs_ReturnsCorrectSuit(input: "Spades", expectedSuit: Spades)
+DeuxCentsCardGame.Tests.Managers.TrumpSelectionManagerTests.SelectTrumpSuit_VariousInputs_ReturnsCorrectSuit(input: "Hearts", expectedSuit: Hearts)
+DeuxCentsCardGame.Tests.Managers.TrumpSelectionManagerTests.SelectTrumpSuit_VariousInputs_ReturnsCorrectSuit(input: "Diamonds", expectedSuit: Diamonds)
+DeuxCentsCardGame.Tests.Managers.TrumpSelectionManagerTests.SelectTrumpSuit_VariousInputs_ReturnsCorrectSuit(input: "Clubs", expectedSuit: Clubs)
+DeuxCentsCardGame.Tests.Managers.TrumpSelectionManagerTests.SelectTrumpSuit_ReturnsValidSuit
+
+
 // next
 // Currently at 5. Unity Naming Convention Adjustments
 //
