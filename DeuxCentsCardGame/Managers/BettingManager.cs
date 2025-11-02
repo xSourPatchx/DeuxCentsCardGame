2832880ce25fb04dec5bac8697e018d74761e180
Pat <60205594+xSourPatchx@users.noreply.github.com>
2025-11-02 22:48:51 +0000

    Refactored BettingManager

--- DeuxCentsCardGame/Managers/BettingManager.cs
+++ DeuxCentsCardGame/Managers/BettingManager.cs
@@ -45,21 +45,286 @@ namespace DeuxCentsCardGame.Managers
 
             while (!IsBettingRoundComplete)
             {
-                IsBettingRoundComplete = HandleBettingRound(startingIndex);
+                IsBettingRoundComplete = ProcessSingleBettingRound(startingIndex);
             }
         }
 
         private void CompleteBettingRound()
         {
             DetermineWinningBid();
+            RaiseBettingCompletedEvent();
+        }
 
+        private bool ProcessSingleBettingRound(int startingIndex)
+        {
+            for (int i = 0; i < _players.Count; i++)
+            {
+                int currentPlayerIndex = (startingIndex + i) % _players.Count;
+                Player currentPlayer = _players[currentPlayerIndex];
+
+                // Skip players who have already passed
+                if (SkipPlayer(currentPlayer))
+                    continue;
+
+                // Parse and handle user input
+                if (ProcessPlayerBid(currentPlayerIndex))
+                    return true;
+
+                // Check if three or more players have passed
+                if (EndBettingRound())
+                    return HandleThreePassesScenario();
+            }
+            return false;
+        }
+
+        private bool SkipPlayer(Player player)
+        {
+            return player.HasPassed;
+        }
+
+        private bool EndBettingRound()
+        {
+            int passedPlayersCount = _players.Count(pass => pass.HasPassed);
+            return passedPlayersCount >= _gameConfig.MinimumPlayersToPass;
+        }
+
+        private bool ProcessPlayerBid(int currentPlayerIndex)
+        {
+            while (true)
+            {
+                string betInput = RequestBetInput(currentPlayerIndex);
+
+                if (IsPassInput(betInput))
+                {
+                    HandlePassInput(currentPlayerIndex);
+                    return false;
+                }
+
+                if (ParseAndValidateBet(betInput, out int bet))
+                {
+                    return HandleValidBet(currentPlayerIndex, bet);
+                }
+
+                RaiseInvalidBetEvent(currentPlayerIndex);
+            }
+        }
+
+        private string RequestBetInput(int playerIndex)
+        {
+            return _eventManager.RaiseBetInput(
+                                _players[playerIndex],
+                                _gameConfig.MinimumBet,
+                                _gameConfig.MaximumBet,
+                                _gameConfig.BetIncrement
+            );
+        }
+
+        private bool IsPassInput(string input)
+        {
+            return input == "pass";
+        }
+
+        private bool ParseAndValidateBet(string betInput, out int bet)
+        {
+            if (!int.TryParse(betInput, out bet))
+                return false;
+
+            return IsValidBet(bet);
+        }
+
+        private void RaiseInvalidBetEvent(int playerIndex)
+        {
+            _eventManager.RaiseInvalidMove(
+            _players[playerIndex],
+            "Invalid bet, please try again.",
+            InvalidMoveType.InvalidBet
+            );
+        }
+
+        private bool IsValidBet(int bet)
+        {
+            return IsBetInValidRange(bet) &&
+                   IsBetValidIncrement(bet) &&
+                   IsBetUnique(bet);
+        }
+
+        private bool IsBetInValidRange(int bet)
+        {
+            return bet >= _gameConfig.MinimumBet && bet <= _gameConfig.MaximumBet;
+        }
+
+        private bool IsBetValidIncrement(int bet)
+        {
+            return bet % _gameConfig.BetIncrement == 0;
+        }
+
+        private bool IsBetUnique(int bet)
+        {
+            return !_players.Any(player => player.CurrentBid == bet);
+        }
+
+        private void HandlePassInput(int currentPlayerIndex)
+        {
+            Player player = _players[currentPlayerIndex];
+
+            MarkPlayerAsPassed(player);
+            RaiseBettingActionEvent(player, player.CurrentBid, isPassed: true, player.HasBet);
+        }
+
+        private void MarkPlayerAsPassed(Player player)
+        {
+            player.HasPassed = true;
+
+            if (!player.HasBet)
+            {
+                player.CurrentBid = -1;
+            }
+        }
+
+        private bool HandleValidBet(int currentPlayerIndex, int bet)
+        {
+            Player player = _players[currentPlayerIndex];
+
+            RecordPlayerBet(player, bet);
+            RaiseBettingActionEvent(player, bet, isPassed: false, hasBet: true);
+
+            if (IsMaximumBet(bet))
+            {
+                // End round immediately
+                return HandleMaximumBetScenario(currentPlayerIndex);
+            }
+
+            return false;
+        }
+
+        private void RecordPlayerBet(Player player, int bet)
+        {
+            player.CurrentBid = bet;
+            player.HasBet = true;
+        }
+
+        private bool IsMaximumBet(int bet)
+        {
+            return bet == _gameConfig.MaximumBet;
+        }
+
+        private bool HandleMaximumBetScenario(int playerIndex)
+        {
+            ForceOtherPlayersToPass(playerIndex);
+            return true;
+        }
+
+        private void ForceOtherPlayersToPass(int maxBetPlayerIndex)
+        {
+            for (int i = 0; i < _players.Count; i++)
+            {
+                if (i == maxBetPlayerIndex || _players[i].HasPassed)
+                    continue;
+                
+                Player otherPlayer = _players[i];
+                MarkPlayerAsPassed(otherPlayer);
+                RaiseBettingActionEvent(otherPlayer, otherPlayer.CurrentBid, isPassed: true, otherPlayer.HasBet);
+            }
+        }
+
+        private bool HandleThreePassesScenario()
+        {
+            List<Player> activePlayers = GetActivePlayers();
+
+            if (CheckIfOnlyOnePlayerRemains(activePlayers))
+            {
+                HandleSingleActivePlayer(activePlayers[0]);
+            }
+
+            return true;
+        }
+
+        private List<Player> GetActivePlayers()
+        {
+            return _players.Where(player => !player.HasPassed).ToList();
+        }
+
+        private bool CheckIfOnlyOnePlayerRemains(List<Player> activePlayers)
+        {
+            return activePlayers.Count == 1;
+        }
+
+        private void HandleSingleActivePlayer(Player player)
+        {
+            if (NoBetsPlaced())
+            {
+                ForceMinimumBet(player);
+            }
+            else if (!player.HasBet)
+            {
+                ForcePlayerToPass(player);
+            }
+        }
+
+        private bool NoBetsPlaced()
+        {
+            return !_players.Any(p => p.HasBet && p.CurrentBid > 0);
+        }
+
+        private void ForceMinimumBet(Player player)
+        {
+            player.HasBet = true;
+            player.CurrentBid = _gameConfig.MinimumBet;
+            RaiseBettingActionEvent(player, _gameConfig.MinimumBet, isPassed: false, hasBet: true);
+        }
+        
+        private void ForcePlayerToPass(Player player)
+        {
+            player.HasPassed = true;
+            player.CurrentBid = -1;
+            RaiseBettingActionEvent(player, -1, isPassed: true, hasBet: false);    
+        }
+
+        private void DetermineWinningBid()
+        {
+            var validBids = GetValidBids();
+
+            if (validBids.Any())
+            {
+                SetWinningBid(validBids);
+            }
+            else
+            {          
+                ClearWinningBid();
+            }
+        }
+
+        private IEnumerable<Player> GetValidBids()
+        {
+            return _players.Where(player => player.CurrentBid > 0);
+        }
+
+        private void SetWinningBid(IEnumerable<Player> validBids)
+        {
+            CurrentWinningBid = validBids.Max(player => player.CurrentBid);
+            CurrentWinningBidIndex = _players.FindIndex(player => player.CurrentBid == CurrentWinningBid);
+        }
+        
+        private void ClearWinningBid()
+        {
+            CurrentWinningBid = 0;
+            CurrentWinningBidIndex = -1;
+        }
+
+        private void RaiseBettingCompletedEvent()
+        {
             if (CurrentWinningBid > 0)
             {
                 Player winningBidder = _players[CurrentWinningBidIndex];
                 var allBids = _players.ToDictionary(player => player, player => player.CurrentBid);
-        
+
                 _eventManager.RaiseBettingCompleted(winningBidder, CurrentWinningBid, allBids);
             }
+        }
+
+        private void RaiseBettingActionEvent(Player player, int bid, bool isPassed, bool hasBet)
+        {
+            _eventManager.RaiseBettingAction(player, bid, isPassed, hasBet);
         }
 
         public void ResetBettingRound()
@@ -77,167 +342,6 @@ namespace DeuxCentsCardGame.Managers
         public void UpdateDealerIndex(int newDealerIndex)
         {
             _dealerIndex = newDealerIndex;
-        }
-
-        private bool HandleBettingRound(int startingIndex)
-        {
-            for (int i = 0; i < _players.Count; i++)
-            {
-                int currentPlayerIndex = (startingIndex + i) % _players.Count;
-                Player currentPlayer = _players[currentPlayerIndex];
-
-                // Skip players who have passed
-                if (currentPlayer.HasPassed)
-                    continue;
-
-                // Parse and handle user input
-                if (HandlePlayerBids(currentPlayerIndex))
-                    return true; // End betting round
-
-                // Check if three or more players have passed
-                if (_players.Count(pass => pass.HasPassed) >= _gameConfig.MinimumPlayersToPass)
-                {
-                    return HandleThreePassesScenario();   
-                }
-            }
-
-            return false;
-        }
-
-        private bool HandlePlayerBids(int currentPlayerIndex)
-        {
-            while (true)
-            {
-                string betInput = _eventManager.RaiseBetInput(
-                                _players[currentPlayerIndex], 
-                                _gameConfig.MinimumBet, 
-                                _gameConfig.MaximumBet, 
-                                _gameConfig.BetIncrement
-                );
-
-                if (betInput == "pass")
-                {
-                    HandlePassInput(currentPlayerIndex);
-                    return false;
-                }
-
-                if (int.TryParse(betInput, out int bet) && IsValidBet(bet))
-                {
-                    return HandleValidBet(currentPlayerIndex, bet);
-                }
-
-                _eventManager.RaiseInvalidMove(
-                    _players[currentPlayerIndex],
-                    "Invalid bet, please try again.",
-                    InvalidMoveType.InvalidBet
-                );
-            }
-        }
-
-        private void HandlePassInput(int currentPlayerIndex)
-        {
-            var player = _players[currentPlayerIndex];
-            player.HasPassed = true;
-
-            // Only set to -1 if they haven't placed a bet yet
-            if (!player.HasBet)
-            {
-                player.CurrentBid = -1;
-            }
-
-            _eventManager.RaiseBettingAction(player, player.CurrentBid, true, player.HasBet);
-        }
-
-        private bool IsValidBet(int bet)
-        {
-            return bet >= _gameConfig.MinimumBet &&
-                   bet <= _gameConfig.MaximumBet &&
-                   bet % _gameConfig.BetIncrement == 0 &&
-                   !_players.Any(player => player.CurrentBid == bet);
-        }
-
-        private bool HandleValidBet(int currentPlayerIndex, int bet)
-        {
-            var player = _players[currentPlayerIndex];
-            player.CurrentBid = bet;
-            player.HasBet = true;
-
-            _eventManager.RaiseBettingAction(player, bet, false, true);
-
-            if (bet == _gameConfig.MaximumBet)
-            {
-                return HandleMaximumBetScenario(currentPlayerIndex); // End round immediately
-            }
-
-            return false;
-        }
-
-        private bool HandleMaximumBetScenario(int playerIndex)
-        {
-            // Force all other players to pass when someone bets 100
-            for (int otherPlayerIndex = 0; otherPlayerIndex < _players.Count; otherPlayerIndex++)
-            {
-                if (otherPlayerIndex != playerIndex && !_players[otherPlayerIndex].HasPassed)
-                {
-                    var otherPlayer = _players[otherPlayerIndex];
-                    otherPlayer.HasPassed = true;
-
-                    // Preserve player existing bid if they had one
-                    if (!otherPlayer.HasBet)
-                    {
-                        otherPlayer.CurrentBid = -1;
-                    }
-
-                    _eventManager.RaiseBettingAction(otherPlayer, otherPlayer.CurrentBid, true, otherPlayer.HasBet);
-                }
-            }
-
-            return true;
-        }
-
-        private bool HandleThreePassesScenario()
-        {
-            var activePlayers = _players.Where(pass => !pass.HasPassed).ToList();
-            
-            // Check if three players have passed and one remains
-            if (activePlayers.Count == 1)
-            {
-                // Check if this is the "first three pass" scenario or no one has placed a bet yet
-                if (!_players.Any(p => p.HasBet && p.CurrentBid > 0))
-                {
-                    // Force the last player to bet 50
-                    activePlayers[0].HasBet = true;
-                    activePlayers[0].CurrentBid = _gameConfig.MinimumBet;
-
-                    _eventManager.RaiseBettingAction(activePlayers[0], _gameConfig.MinimumBet, false, true);
-                }
-                // not sure if this is needed, but just in case
-                else if (!activePlayers[0].HasBet)
-                {
-                    activePlayers[0].HasPassed = true;
-                    activePlayers[0].CurrentBid = -1;
-                    _eventManager.RaiseBettingAction(activePlayers[0], -1, true, false);
-                }
-            }
-            
-            return true;
-        }
-
-        private void DetermineWinningBid()
-        {
-            // Only consider valid bids (> 0)
-            var validBids = _players.Where(player => player.CurrentBid > 0);
-            
-            if (validBids.Any())
-            {
-                CurrentWinningBid = validBids.Max(player => player.CurrentBid);
-                CurrentWinningBidIndex = _players.FindIndex(player => player.CurrentBid == CurrentWinningBid);
-            }
-            else
-            {
-                CurrentWinningBid = 0;
-                CurrentWinningBidIndex = -1;
-            }
         }
     }
 }
--- TempFileCanDelete.cs
+++ TempFileCanDelete.cs
@@ -1,68 +1,4 @@
-// ## 1. Can you help me with myScoringManager.cs Refactoring
-// **Problem:**
-// - Too many responsibilities
-// - Hard to test individual logic
-// - Difficult to understand flow
-// **Desired Solution:**
-// ScoreTeam
-// ├── GetTeamRoundPoints (1 line)
-// ├── GetTeamTotalPoints (1 line)
-// ├── DetermineIfTeamCannotScore (5 lines)
-// ├── RaiseTeamScoringEvent (7 lines)
-// ├── ApplyPointsToTeam (7 lines)
-// └── CalculateAwardedPoints
-//     └── CalculateBidWinnerPoints (1 line)
-
-// ---
-
-// ## 2. Can you help me with my GameController class Refactoring
-// Problem: Multiple complex methods
-// - `NewRound` (11 lines of mixed concerns)
-// - `PlaySingleTrick` (10 lines, complex flow)
-// - `PlayPlayerTurn` (15 lines, multiple responsibilities)
-// - `GetValidCardFromPlayer` (15 lines, validation + UI)
-
-// Desired Solution: Hierarchical decomposition
-// NewRound (orchestrator)
-// ├── InitializeRound
-// │   └── ResetRound
-// ├── PrepareGameState
-// │   ├── ShuffleDeck
-// │   └── DealCards
-// ├── ExecuteRoundPhases
-// │   ├── ExecuteBettingRound
-// │   ├── SelectTrumpSuit
-// │   └── PlayRound
-// │       └── PlayAllTricks
-// │           └── ExecuteSingleTrick
-// │               ├── PlayTrickCards
-// │               │   └── ExecutePlayerTurn
-// │               │       ├── RaisePlayerTurnEvent
-// │               │       ├── GetValidCardFromPlayer
-// │               │       ├── UpdateLeadingSuit
-// │               │       └── RaiseCardPlayedEvent
-// │               ├── DetermineTrickWinner
-// │               ├── CalculateTrickPoints
-// │               └── AwardTrickPointsAndNotify
-// └── FinalizeRound
-//     ├── ScoreRound
-//     ├── EndGameCheck
-//     └── RotateDealer
-
-// #### `GetValidCardFromPlayer` → Break into:
-// - `RequestCardSelection()` - Gets user input
-// - `IsCardValid()` - Validates card
-// - `DisplayInvalidCardMessage()` - Shows error
-
-// #### `ExecutePlayerTurn` → Break into:
-// - `RaisePlayerTurnEvent()` - Event notification
-// - `GetValidCardFromPlayer()` - Card selection
-// - `UpdateLeadingSuit()` - Game state update
-// - `RaiseCardPlayedEvent()` - Event notification
-
-// ---
-
-// ## 3. Can you help me with my BettingManager.cs Refactoring
+// ## Can you help me with my BettingManager.cs refactoring
 // **Problems: `HandleBettingRound`**
 // - Complex nested logic
 // - Multiple exit points
@@ -104,16 +40,14 @@
 //     │   └── ClearWinningBid
 //     └── RaiseBettingCompletedEvent
 
-// ### Key Improvements:
-// #### Validation Split:
-// // Problem: Complex condition
+// Problem: Complex condition
 // if (int.TryParse(betInput, out int bet) && 
 //     bet >= _gameConfig.MinimumBet &&
 //     bet <= _gameConfig.MaximumBet &&
 //     bet % _gameConfig.BetIncrement == 0 &&
 //     !_players.Any(player => player.CurrentBid == bet))
 
-// // Solution: Clear, testable methods
+// Solution: Clear, testable methods
 // private bool IsValidBet(int bet)
 // {
 //     return IsBetInValidRange(bet) && 
@@ -124,7 +58,7 @@
 
 // ## Additional Refactoring Opportunities
 
-// ### 1. **Card.cs - `WinsAgainst` method**
+// **Card.cs - `WinsAgainst` method**
 // Current method has 5 nested conditions. Could be extracted:
 // WinsAgainst
 // ├── BothAreTrump
@@ -133,41 +67,17 @@
 // ├── BothMatchLeadingSuit
 // └── CompareFaceValues
 
-// ### 2. **GameEventHandler.cs**
+// **GameEventHandler.cs**
 // Some display methods could be simplified:
 // OnBettingCompleted
 // ├── FormatBidText (extract bid display logic)
 // ├── DisplayBiddingResults
 // └── ShowWinnerHand
 
-// ### 3. **DeckManager.cs - `ShuffleDeck`**
+// **DeckManager.cs - `ShuffleDeck`**
 // Fisher-Yates shuffle could be extracted:
 // ShuffleDeck
 // ├── LogShuffleStart
 // └── PerformFisherYatesShuffle
 
 // ---
-
-// ## Refactoring Principles Applied
-
-// ### 1. **Single Responsibility Principle (SRP)**
-// Each method does one thing well
-
-// ### 2. **Extract Method Pattern**
-// - Pull complex logic into named methods
-// - Method names document intent
-// - Parameters make dependencies explicit
-
-// ### 3. **Composition over Complexity**
-// - Small methods compose to create complex behavior
-// - Easy to rearrange and modify
-
-// ### 4. **Self-Documenting Code**
-// - Method names replace comments
-// - Clear what each piece does
-// - Reduces cognitive load
-
-// ### 5. **Testability**
-// - Small methods are easy to unit test
-// - Mock/stub individual components
-// - Test edge cases in isolation
